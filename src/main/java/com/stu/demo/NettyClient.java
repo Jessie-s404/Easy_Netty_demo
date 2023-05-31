2405be71cffacfeb4a8fa532c65beda5d33f79bd
shenjiaqi <2078389729@qq.com>
2023-05-31 14:16:05 +0800

    easy netty demo

---
+++ src/main/java/com/stu/demo/NettyClient.java
@@ -0,0 +1,56 @@
+package com.stu.demo;
+
+/**
+ * ClassName: NettyClient
+ * Description:
+ *
+ * @Author: shenjiaqi
+ * 编辑于：2023-05-31 上午11:11   @Version 1.0        描述
+ */
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * Netty客户端
+ */
+public class NettyClient {
+    public static void main(String[] args) throws InterruptedException {
+        //1.创建线程组
+        EventLoopGroup group = new NioEventLoopGroup();
+        //2.创建客户端启动助手
+        Bootstrap bootstrap = new Bootstrap();
+        //3.设置线程组
+        bootstrap.group(group)
+                .channel(NioSocketChannel.class)//4.设置客户端通道实现为NIO
+                .handler(new ChannelInitializer<SocketChannel>() {//5.创建一个通道初始化对象
+
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        //6.向pipeline中添加自定义业务处理handler
+                        ch.pipeline().addLast(new NettyClientHandle());
+                    }
+                });
+        //7.启动客户端，等待连接服务端，同时将异步改为同步
+        ChannelFuture future = bootstrap.connect("127.0.0.1", 9999).sync();
+        future.addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture channelFuture) throws Exception {
+                if (future.isSuccess()) {
+                    System.out.println("数据发送成功.");
+                } else {
+                    System.out.println("数据发送失败.");
+                }
+            }
+        });
+        //8.关闭通道和关闭连接池
+        future.channel().closeFuture().sync();
+        group.shutdownGracefully();
+    }
+}
---
+++ src/main/java/com/stu/demo/NettyClientHandle.java
@@ -0,0 +1,80 @@
+package com.stu.demo;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandler;
+import io.netty.util.CharsetUtil;
+
+/**
+ * ClassName: NettyClientHandle
+ * Description:
+ *
+ * @Author: shenjiaqi
+ * 编辑于：2023-05-31 上午11:34   @Version 1.0        描述
+ */
+public class NettyClientHandle implements ChannelInboundHandler {
+
+    /**
+     * 通道就绪事件
+     *
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(Unpooled.copiedBuffer("你好呀，我是Netty客户端", CharsetUtil.UTF_8));
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf byteBuf = (ByteBuf) msg;
+        System.out.println("服务端发来消息：" + byteBuf.toString(CharsetUtil.UTF_8));
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+
+    }
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+
+    }
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+
+    }
+
+    @Override
+    public void channelWritabilityChanged(ChannelHandlerContext ctx) throws Exception {
+
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+
+    }
+
+}
